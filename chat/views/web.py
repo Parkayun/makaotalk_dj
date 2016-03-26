a8621540d328fea7fec7184e36079c9dcaa04e93
Parkayun <iamparkayun@gmail.com>
2016-03-26 14:29:49 +0900

    Add showing chat rooms feature to index page

--- chat/views/web.py
+++ chat/views/web.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from ..models import ChatRoom
+
 
 def index(request):
-    return render(request, "chat/index.html")
+    chat_rooms = ChatRoom.objects.all().order_by('-id')
+    data = {'chat_rooms': chat_rooms}
+    return render(request, "chat/index.html", data)
