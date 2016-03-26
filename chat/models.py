add1eb36e60f92adb72be53c015cbdb53f828460
Parkayun <iamparkayun@gmail.com>
2016-03-26 14:50:07 +0900

    Add ChatRoom description

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class ChatRoom(models.Model):
+    """ChatRoom model contains basic information for chat.
+    The chat feature is working by each ChatRoom's primary key(id).
+    """
     title = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
